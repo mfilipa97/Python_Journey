3a4a4304d581282f93d4c2dd43bec220d87decb7
John Doe <marrian1997@hotmail.com>
2025-02-06 13:38:31 +0000

    Arithmetic & math

---
+++ Learning/Math.py
@@ -0,0 +1,48 @@
+import math # import to use some math functions such ah math.pi, math.sqrt
+
+friends = 5
+friends = friends + 1 # same as bellow
+friends += 1 #same as ^
+
+friends = friends -2
+friends -= 2
+
+friends = friends * 3
+friends *= 3
+
+friends = friends  /2
+friends /= 2
+
+friends = friends ** 2
+friends **= 2
+
+remainder = friends % 2 # Modular operator used a lot to find even or odd number
+
+print(remainder)
+
+x = 3.14
+y = 4
+z = 5
+
+result = round(x)
+absolute = abs(y)
+power = pow(3,4)
+maxVal = max(x,y,z)
+minVal = min(x,y,z)
+
+print(minVal)
+print(maxVal)
+print(power)
+print (result)
+print (absolute)
+
+print (math.pi)
+
+square = math.sqrt(x) #square root
+print(square)
+
+roundUp = math.ceil(x) #round up a number
+print(roundUp)
+
+roundDown = math.floor(x)
+print(roundDown)
