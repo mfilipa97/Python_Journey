9eefa16dd8fcdf9e583fb1a92a13bba359908cfb
John Doe <marrian1997@hotmail.com>
2025-02-09 19:44:45 +0000

    Updated result on For Loops.py

--- Learning/For Loops.py
+++ Learning/For Loops.py
@@ -1,25 +1,25 @@
 # for loops =  execute a block of code a fixed number of time
 #   You can iterate over a range, string, sequence, tc.
 
-#Count from 1 to 10
+#Count from 1 to 10 RESULT - 1 2 3 4 5 6 7 8 9 10
 for counter in range(1,11):
     print(counter)
-#Count in reverse enter reversed (range(-)
+#Count in reverse enter reversed (range(-) RESULT 10 9 8 7 6 5 4 3 2 1
 for reverseCounter in reversed(range(1,11)):
     print(reverseCounter)
 print("Happy New Year")
 
-#Count in 2 ( based on what you choose)
-for counterIn2 in range(1,11, 2): # 2 is the chosen numnber
+#Count in 2 ( based on what you choose) RESULT 1 3 5 7 9
+for counterIn2 in range(1,11, 2): # 2 is the chosen number
     print(counterIn2)
 
-#Skipping a chosen number
+#Skipping a chosen number RESULT 1 2 3 4 5 6 7 8 9 10 11 12 14 until 21
 for countTo20 in range (1, 21):
     if countTo20 == 13: #choosen number
         continue
     else:
         print (countTo20)
-# When it reached a chosen number it'll stop.
+# When it reached a chosen number it'll stop. RESULT 1 2 3 4 5 6 7 8 9 10 11 12 13 STOP
 for countTo20 in range(1, 21):
     if countTo20 == 13: #choosen number
         break
